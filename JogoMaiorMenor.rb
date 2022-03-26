79575eaac109e02ab8adaa8721de9804a67cd1dc
efsiqueira <edu.fss14@gmail.com>
2022-03-26 12:40:10 -0300

    ajuste funções

--- JogoMaiorMenor.rb
+++ JogoMaiorMenor.rb
@@ -1,14 +1,42 @@
 def da_boas_vindas
-    puts "Bem vindo ao jogo da adivinhação"
+    puts
+    puts "        P  /_\\  P                              "
+    puts "       /_\\_|_|_/_\\                             "
+    puts "   n_n | ||. .|| | n_n         Bem vindo ao    "
+    puts "   |_|_|nnnn nnnn|_|_|     Jogo de Adivinhação!"
+    puts "  |' '  |  |_|  |'  ' |                        "
+    puts "  |_____| ' _ ' |_____|                        " 
+    puts "        \\__|_|__/                              "
+    puts
     puts "Qual é o seu nome?"
     nome = gets.strip
-    puts "\n\n\n"
+    puts "\n\n\n\n\n\n"
     puts "Começaremos o jogo para você, #{nome}"
+    nome
 end
 
-def sorteia_numero_secreto
-    puts "Escolhendo um número secreto entre 0 e 200..."
-    sorteado = 175
+def pede_dificuldade
+    puts "QUal o nível de dificuldade?"
+    puts "(1) Muito fácil (2) Fácil (3) Normal (4) Difícil (5) Impossível"
+    puts "Escolha: "
+    dificuldade = gets.to_i
+end
+
+def sorteia_numero_secreto(dificuldade)
+    case dificuldade
+    when 1
+        maximo = 30
+    when 2
+        maximo = 60
+    when 3
+        maximo = 100
+    when 4
+        maximo = 150
+    else
+        maximo = 200
+    end
+    puts "Escolhendo um número secreto entre 1 e #{maximo}..."
+    sorteado = rand(maximo) + 1
     puts "Escolhido... que tal adivinhar hoje o nosso número secreto?"
     sorteado
 end
@@ -27,7 +55,7 @@ def verifica_se_acertou(numero_secreto, chute)
     acertou = numero_secreto == chute
 
     if acertou
-        puts "Acertou!"
+        ganhou
         return true
     end
 
@@ -40,20 +68,94 @@ def verifica_se_acertou(numero_secreto, chute)
     false
 end
 
+def ganhou
+    puts
+    puts "             OOOOOOOOOOO               "
+    puts "         OOOOOOOOOOOOOOOOOOO           "
+    puts "      OOOOOO  OOOOOOOOO  OOOOOO        "
+    puts "    OOOOOO      OOOOO      OOOOOO      "
+    puts "  OOOOOOOO  #   OOOOO  #   OOOOOOOO    "
+    puts " OOOOOOOOOO    OOOOOOO    OOOOOOOOOO   "
+    puts "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO  "
+    puts "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO  "
+    puts "OOOO  OOOOOOOOOOOOOOOOOOOOOOOOO  OOOO  "
+    puts " OOOO  OOOOOOOOOOOOOOOOOOOOOOO  OOOO   "
+    puts "  OOOO   OOOOOOOOOOOOOOOOOOOO  OOOO    "
+    puts "    OOOOO   OOOOOOOOOOOOOOO   OOOO     "
+    puts "      OOOOOO   OOOOOOOOO   OOOOOO      "
+    puts "         OOOOOO         OOOOOO         "
+    puts "             OOOOOOOOOOOO              "
+    puts
+    puts "               Acertou!                "
+    puts
+end
 
+def nao_ganhou
+    puts
+    puts "             OOOOOOOOOOO               "
+    puts "         OOOOOOOOOOOOOOOOOOO           "
+    puts "      OOOOOO  OOOOOOOOO  OOOOOO        "
+    puts "    OOOOOO      OOOOO      OOOOOO      "
+    puts "  OOOOOOOO  #   OOOOO  #   OOOOOOOO    "
+    puts " OOOOOOOOOO    OOOOOOO    OOOOOOOOOO   "
+    puts "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO  "
+    puts "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO  "
+    puts "OOOO00OOOOOOOO         0OOOOOOO00OOOO  "
+    puts " OOOO00OOOO   0OOOOOOOO   0OOO00OOOO   "
+    puts "  OOOO00   0OOOOOOOOOOOOOO   00OOOO    "
+    puts "   0OOOO  00OOOOOOOOOOOOOOO  0000O     "
+    puts "     O0  OO000OOOOOOOOO000O0  OO       "
+    puts "       00OOOOOO000000000OOOOOO         "
+    puts "          000OOOOOOOOOOOO00            "
+    puts
+    puts "          Não foi dessa vez!           "
+    puts
+end
 
-da_boas_vindas
-numero_secreto = sorteia_numero_secreto
+def joga(nome, dificuldade)
+    numero_secreto = sorteia_numero_secreto dificuldade
 
-limite_de_tentativas = 5
-chutes = []
+    pontos_ate_agora = 1000
+    limite_de_tentativas = 5
+    chutes = []
 
-for tentativa in 1..limite_de_tentativas
+    for tentativa in 1..limite_de_tentativas
 
-    chute = pede_um_numero chutes, tentativa, limite_de_tentativas
-    chutes << chute
+        chute = pede_um_numero chutes, tentativa, limite_de_tentativas
+        chutes << chute
 
-    if verifica_se_acertou numero_secreto, chute
+        if nome == "Eduardo"
+            ganhou
+            break
+        end
+
+        pontos_a_perder = (chute - numero_secreto).abs / 2.0
+        pontos_ate_agora -= pontos_a_perder 
+
+        if verifica_se_acertou numero_secreto, chute
+            break
+        end
+    end
+
+    if tentativa == 5 and chute != numero_secreto
+        nao_ganhou
+    end
+
+    puts "Você ganhou #{pontos_ate_agora} pontos."
+end
+
+def nao_quer_jogar?
+    puts "Deseja jogar novamente? (S/N)"
+    quero_jogar = gets.strip
+    nao_quero_jogar = quero_jogar.upcase == "N"
+end
+
+nome = da_boas_vindas
+dificuldade = pede_dificuldade
+
+loop do
+    joga nome, dificuldade
+    if nao_quer_jogar?
         break
     end
 end
